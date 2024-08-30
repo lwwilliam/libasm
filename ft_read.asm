2da0e1c418dbdda4e09186877d1ba17385eeb93a
lwwilliam <lee1052william@gmail.com>
2024-08-30 17:07:49 +0800

    fix ft_read indentation

--- ft_read.asm
+++ ft_read.asm
@@ -4,23 +4,23 @@ global _ft_read
 extern __errno_location
 
 %ifdef __APPLE__
-%define SYS_READ 0x2000003   ; syscall number for write in macOS
+%define SYS_READ 0x2000003  ; syscall number for write in macOS
 %else
-%define SYS_READ 0           ; syscall number for write in Linux
+%define SYS_READ 0          ; syscall number for write in Linux
 %endif
 
 _ft_read:
-	mov rax, SYS_READ		; syscall number for sys_read
-	mov rbx, rdi			; rbx = fd
-	mov rcx, rsi			; rcx = buffer
-	mov rdx, rdx			; rdx = len
+    mov rax, SYS_READ       ; syscall number for sys_read
+    mov rbx, rdi            ; rbx = fd
+    mov rcx, rsi            ; rcx = buffer
+    mov rdx, rdx            ; rdx = len
 
-    syscall         		; call kernel
+    syscall                 ; call kernel
 
-	test rax, rax
-	js _return
+    test rax, rax
+    js _return
 
-	ret
+    ret
 
 _return:
     neg rax
